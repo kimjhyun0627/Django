1d03d86841a19d0d119de99ef6232c608fb5e62f
kimjhyun0627 <kimjhyun0627@gmail.com>
2022-05-15 23:58:48 +0900

    test

--- blog/views.py
+++ blog/views.py
@@ -57,7 +57,7 @@ def category_page(request, slug):
         post_list = Post.objects.filter(category=None)
     else:
         category = Category.objects.get(slug=slug)
-        post_list = Post.objects.filter(category=category) 
+        post_list = Post.objects.filter(category=category)
         
     return render(
         request,
